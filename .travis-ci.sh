fc0c8d76e3478c4f4508c03135a502a09504cc89
Peter Newman <peternewman@users.noreply.github.com>
2016-01-18 20:21:45 +0000

    Skip "E114 indentation is not a multiple of four (comment)"

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,9 +4,9 @@
 # It runs the appropriate commands depending on the task requested.
 
 if [[ $TASK = 'flake8' ]]; then
-  flake8 --max-line-length 80 --exclude *_pb2.py,.git,__pycache --ignore E111,E121,E129 config_helper.py master.cfg pass_toucher.py
+  flake8 --max-line-length 80 --exclude *_pb2.py,.git,__pycache --ignore E111,E114,E121,E129 config_helper.py master.cfg pass_toucher.py
   # build.config has to be checked seperately, as it's not strictly a Python file
-  flake8 --max-line-length 80 --exclude *_pb2.py,.git,__pycache --ignore E111,E121,E129,F821 build.config
+  flake8 --max-line-length 80 --exclude *_pb2.py,.git,__pycache --ignore E111,E114,E121,E129,F821 build.config
 elif [[ $TASK = 'pychecker' ]]; then
   # Can't check these files as they aren't true .py files: build.config master.cfg 
   pychecker --quiet --maxargs 12 config_helper.py pass_toucher.py
